0c1a77302a445b033a709a85abde6f5de2c17368
xiongchiamiov <xiong.chiamiov@gmail.com>
2013-08-25 19:41:36 -0700

    Add first hack at client-side... client.
    Basically, I took the Jinja template I wrote previously and stuck it inside an
    AJAX onSuccess function.  It's still ugly, but that means you can include this
    file wherever you want to embed the data.
    
    And yeah, it went back to being hardcoded to Taylor's report.

---
+++ portfolinator.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $.ajax({
+        url: 'http://ani.pe/portfolinator/user/mutantfreak',
+        type: 'GET',
+        dataType: 'jsonp',
+        success: function(json) {
+            console.log(json);
+            var repos = $('<div>').addClass('portfolinator-repos');
+            $.each(json.repos, function(i, repo) {
+                var div = $('<div>').addClass('portfolinator-repo');
+                div.append(
+                    $('<h2>').append(
+                        $('<a>').attr('href', repo.url).text(repo.name)
+                    ),
+                    $('<h3>').text(repo.description),
+                    $('<h3>').append(
+                        $('<a>').attr('href', repo.homepage).text(repo.homepage)
+                    ),
+                    $('<ul>').append(
+                        $('<li>').text('Language: '+repo.language),
+                        $('<li>').text('Created: '+repo.created),
+                        $('<li>').text('Last Pushed: '+repo.lastPushed),
+                        $('<li>').text('Is a Fork?: '+repo.isFork),
+                        $('<li>').text('Contributors: '+repo.numContributors),
+                        $('<li>').text('Watchers: '+repo.numWatchers),
+                        $('<li>').text('Stargazers: '+repo.numStargazers),
+                        $('<li>').text('Forks: '+repo.numForks),
+                        $('<li>').text('Total Issues: '+repo.numTotalIssues),
+                        $('<li>').text('Open Issues: '+repo.numOpenIssues),
+                        $('<li>').text('Tags: '+repo.numTags),
+                        $('<li>').text('Contributed Commits: '+repo.numContributedCommits)
+                    )
+                );
+                repos.append(div);
+            });
+            $('#portfolinator').append(
+                $('<h1>').text(json.username),
+                repos
+            );
+        },
+    });
+});
+
